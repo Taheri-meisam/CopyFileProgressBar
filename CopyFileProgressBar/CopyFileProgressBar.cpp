242434dc36b129622f17b1d071e40aad592bc22b
Meisam Taheri <meisam.taheri@inn.no>
2020-12-04 12:16:37 +0100

    bug fix

--- CopyFileProgressBar/CopyFileProgressBar.cpp
+++ CopyFileProgressBar/CopyFileProgressBar.cpp
@@ -1,25 +1,12 @@
 
 
 #include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <string>
-#include <math.h>
-
-void copyFile();
+#include "fileHeader.h"
 
 
-void copyFile() {
-    std::string Destination;
-    std::string Origin;
-    std::string temp;
-    std::string st;
-    char* mem;
-    double d_size = 0;
-    double s_size = 0;
-    double percent = 0;
-    char c = NULL;
-    std::cout << "Enter Origin file Name and address (use '\\\\' ):";
+void copyFile(){
+
+    std::cout << "Enter Origin file address and name (use \\\\ to enter the address exp : e:\\\\file.txt):";
     std::cin >> Origin;
     std::fstream imageFile(Origin, std::ios::in | std::ios::binary);
     imageFile.seekg(0, std::ios::end);
@@ -27,7 +14,7 @@ void copyFile() {
     std::cout << "Size : " << (s_size/1024) << "KB" << std::endl; // print size of the file
     imageFile.clear();
     imageFile.seekg(0, std::ios::beg);
-    std::cout << "where do you want to copy the file (use '\\\\' ):";
+    std::cout << "where do you want to copy the file (use \\\\ to enter the address exp : e:\\\\file.txt ):";
     std::cin >> Destination;
     std::fstream CopyImageFile(Destination, std::ios::out | std::ios::binary);
     if (imageFile && CopyImageFile) {
@@ -68,4 +55,5 @@ void copyFile() {
 int main()
 {
     copyFile();
+    return 0;
 }
---
+++ CopyFileProgressBar/fileHeader.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <math.h>
+
+void copyFile();
+
+std::string Destination;
+std::string Origin;
+std::string temp;
+std::string st;
+char* mem;
+double d_size = 0;
+double s_size = 0;
+double percent = 0;
+char c = NULL;
